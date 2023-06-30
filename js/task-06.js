3d05237fd1946826810d4eb3d51f0be49b0051da
peogeka <peo.geka@gmail.com>
2023-06-30 10:34:16 +0200

    update-task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,14 @@
-const inputEl = document.querySelector("#validation-input");
-const dataLength = inputEl.getAttribute("data-length");
-const checkInputValueLength = (event) => {
-    const currentValue = event.currentTarget.value
-    if (Number(dataLength) === Number(currentValue.length)) {
-        inputEl.classList.add("valid")
-    } else {
-        inputEl.classList.add("invalid")
-    }
-    
-}
-inputEl.addEventListener("blur", checkInputValueLength);
+ const input = document.getElementById('validation-input');
+
+input.addEventListener('blur', function() {
+    const desiredLength = input.getAttribute('data-length');
+    const enteredLength = input.value.length;
+
+if (enteredLength == desiredLength) {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+} else {
+        input.classList.remove('valid');
+        input.classList.add('invalid');
+      }
+});
